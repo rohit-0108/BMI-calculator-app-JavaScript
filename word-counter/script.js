fca8f0b4af484323ca868b47f70dab1af291ac64
Rohit Kolekar <153410103+rohit-0108@users.noreply.github.com>
2024-06-03 08:16:15 +0530

    Delete word-counter directory

--- word-counter/script.js
+++
@@ -1,8 +0,0 @@
-let wordCounts = function (words) {
-    document.getElementById("output").innerHTML =
-      "Words Length is " + words + " 😎";
-  };
-  
-  document.getElementById("btn").onclick = function () {
-    wordCounts(document.getElementById("words").value.length);
-  };
